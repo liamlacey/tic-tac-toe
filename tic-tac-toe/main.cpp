f80a83e696501781ed89348246ad96fbf37ec6b7
Liam Lacey <liamlacey@hotmail.com>
2018-06-20 18:44:43 +0100

    Added empty C++ Xcode project.

---
+++ tic-tac-toe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tic-tac-toe
+//
+//  Created by Liam Lacey on 20/06/2018.
+//  Copyright © 2018 Liam Lacey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
