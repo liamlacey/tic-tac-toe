aafddb13c0fe33f277a47c8d1073a19a1e319d31
Liam Lacey <liamlacey@hotmail.com>
2018-06-22 09:50:15 +0100

    Refactored - split seperate sections of code into functions, corrected TicTacToe instance name.

--- tic-tac-toe/main.cpp
+++ tic-tac-toe/main.cpp
@@ -9,102 +9,30 @@
 #include <iostream>
 #include "TicTacToe.h"
 
-void displayGrid (int gridSize, TicTacToe ticTacToe);
+void displayGrid (const int gridSize, TicTacToe &ticTacToe);
+void choosePosition (const int numOfGridSlots, TicTacToe &ticTacToe);
+bool checkGameStatus (const int gridSize, TicTacToe &ticTacToe);
+void clearAndIgnoreCin();
 
 //==============================================================
 int main(int argc, const char * argv[])
 {
     const auto gridSize = 3;
+    TicTacToe ticTacToe(gridSize);
     const auto numOfGridSlots = gridSize * gridSize;
-    TicTacToe tickTacToe(gridSize);
-    
     auto shouldExit = false;
-
+    
     std::cout << "Welcome to Tic-Tac-Toe!\n";
     std::cout << "Player 1 is 'o', Player 2 is 'x'.\n";
     
-    //main loop that will only exit user requests to
+    //main loop that will only exit if user requests to
     do
     {
-        displayGrid (gridSize, tickTacToe);
+        displayGrid (gridSize, ticTacToe);
         
-        std::cout << "Player " << tickTacToe.getCurrentPlayer() + 1 << ", select a grid position...\n";
+        choosePosition (numOfGridSlots, ticTacToe);
         
-        auto input = 0;
-        auto setGridResult = 0;
-        
-        //loop to set the grid position state, exit once successfully done this
-        do
-        {
-            //loop to get a valid number input, exit once got a valid input
-            do
-            {
-                std::cin >> input;
-                
-                //if not got a valid number input
-                if (!(input >= 1 && input <= numOfGridSlots))
-                {
-                    std::cout << "Invalid input - please try again." << std::endl;
-                    //clear error flag on cin, and ignore anything else on same line
-                    std::cin.clear();
-                    std::cin.ignore(10000,'\n');
-                }
-                
-            } while (!(input >= 1 && input <= numOfGridSlots));
-            
-            setGridResult = tickTacToe.setGridPositionStateForCurrentPlayer(input - 1);
-            
-            //if got an error attempting to set grid position stat
-            if (setGridResult == -1)
-            {
-                std::cout << "The grid position is already being used - please try again." << std::endl;
-            }
-            
-        } while (setGridResult == -1);
-        
-        int gameResult = tickTacToe.checkGameStatus();
-        
-        //If the game is still in play
-        if (gameResult == 0)
-        {
-            tickTacToe.moveToNextPlayer();
-        }
-        else
-        {
-            displayGrid (gridSize, tickTacToe);
-            
-            //if there is now a winner
-            if (gameResult == 1 || gameResult == 2)
-                std::cout << "Player " << gameResult << " has won! Play again? (y/n)" << std::endl;
-            
-            //else all grid positions were used but no winner
-            else
-                 std::cout << "Game over! Play again? (y/n)" << std::endl;
-                
-            
-            char userSelection;
-            
-            //loop to get a valid keyboard response, exit once got a valid response
-            do
-            {
-                std::cin >> userSelection;
-                
-                //if not got a valid number input
-                if (!(userSelection == 'y' || userSelection == 'n'))
-                {
-                    std::cout << "Invalid input - please try again." << std::endl;
-                    //clear error flag on cin, and ignore anything else on same line
-                    std::cin.clear();
-                    std::cin.ignore(10000,'\n');
-                }
-                
-            } while (!(userSelection == 'y' || userSelection == 'n'));
-            
-            if (userSelection == 'y')
-                tickTacToe.resetGame();
-            else
-                shouldExit = true;
-        }
+        shouldExit = checkGameStatus (gridSize, ticTacToe);
         
     } while (!shouldExit);
     
@@ -114,7 +42,7 @@ int main(int argc, const char * argv[])
 }
 
 //==============================================================
-void displayGrid (int gridSize, TicTacToe ticTacToe)
+void displayGrid (const int gridSize, TicTacToe &ticTacToe)
 {
     const auto gridPosHeight = 3;
     auto gridPosCounter = 1;
@@ -169,6 +97,98 @@ void displayGrid (int gridSize, TicTacToe ticTacToe)
     }
     
     std::cout << "\n";
+}
+
+//==============================================================
+void choosePosition (const int numOfGridSlots, TicTacToe &ticTacToe)
+{
+    std::cout << "Player " << ticTacToe.getCurrentPlayer() + 1 << ", select a grid position...\n";
+    
+    auto input = 0;
+    auto setGridResult = 0;
+    
+    //loop to set the grid position state, exit once successfully done this
+    do
+    {
+        //loop to get a valid number input, exit once got a valid input
+        do
+        {
+            std::cin >> input;
+            
+            //if not got a valid number input
+            if (!(input >= 1 && input <= numOfGridSlots))
+            {
+                std::cout << "Invalid input - please try again." << std::endl;
+                clearAndIgnoreCin();
+            }
+            
+        } while (!(input >= 1 && input <= numOfGridSlots));
+        
+        setGridResult = ticTacToe.setGridPositionStateForCurrentPlayer(input - 1);
+        
+        //if got an error attempting to set grid position stat
+        if (setGridResult == -1)
+        {
+            std::cout << "The grid position is already being used - please try again." << std::endl;
+        }
+        
+    } while (setGridResult == -1);
+}
+
+//==============================================================
+bool checkGameStatus (const int gridSize, TicTacToe &ticTacToe)
+{
+    int gameResult = ticTacToe.checkGameStatus();
+    
+    //If the game is still in play
+    if (gameResult == 0)
+    {
+        ticTacToe.moveToNextPlayer();
+    }
+    else
+    {
+        displayGrid (gridSize, ticTacToe);
+        
+        //if there is now a winner
+        if (gameResult == 1 || gameResult == 2)
+            std::cout << "Player " << gameResult << " has won! Play again? (y/n)" << std::endl;
+        
+        //else all grid positions were used but no winner
+        else
+            std::cout << "Game over! Play again? (y/n)" << std::endl;
+        
+        
+        char userSelection;
+        
+        //loop to get a valid keyboard response, exit once got a valid response
+        do
+        {
+            std::cin >> userSelection;
+            
+            //if not got a valid number input
+            if (!(userSelection == 'y' || userSelection == 'n'))
+            {
+                std::cout << "Invalid input - please try again." << std::endl;
+                clearAndIgnoreCin();
+            }
+            
+        } while (!(userSelection == 'y' || userSelection == 'n'));
+        
+        if (userSelection == 'y')
+            ticTacToe.resetGame();
+        else
+            return true;
+    }
+    
+    return false;
+}
+
+//==============================================================
+void clearAndIgnoreCin()
+{
+    //clear error flag on cin, and ignore anything else on same line
+    std::cin.clear();
+    std::cin.ignore(10000,'\n');
 }
 
 
